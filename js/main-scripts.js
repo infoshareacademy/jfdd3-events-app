cf485a2417a1c3f401a7e01d7373e9dfa83bcce4
Msrcin <kurus.mk@gmail.com>
2016-06-14 15:52:17 +0200

    tablica z eventami

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,90 +1,101 @@
 'use strict';
 
-var allClubs = [
-    {name: "Bunkier", city: "Gdansk"},
-    {name: 'Desdemona', city: 'Gdynia'},
-    {name: 'Mewa Towarzyska', city: 'Sopot'},
-    {name: 'Absinthe', city: 'Gdansk'},
-    {name: 'Pokład', city: 'Gdynia'},
-    {name: 'Parlament', city: 'Gdansk'},
-    {name: 'Sfinx 700', city: 'Sopot'},
-    {name: 'Kwadratowa', city: 'Gdansk'},
-    {name: 'Dream Club', city: 'Sopot'},
-    {name: 'Atlantic', city: 'Gdynia'},
-    {name: 'Ucho', city: 'Gdynia'},
-    {name: 'Bunkier', city: 'Gdansk'},
-    {name: 'Makahik', city: 'Sopot'},
-    {name: 'Blues Club', city: 'Gdynia'},
-    {name: 'Universal', city: 'Gdynia'},
-    {name: 'Redlight', city: 'Gdansk'},
-    {name: "B90", city: "Gdansk"},
-    {name: 'Czekoladowa', city: 'Sopot'},
-    {name: 'SPATiF', city: 'Sopot'},
-    {name: 'Protokultura', city: 'Gdansk'},
-    {name: 'Atelier', city: 'Sopot'},
-    {name: 'Żak', city: 'Gdansk'},
-    {name: 'HAH', city: 'Sopot'},
-    {name: 'Lawendowa 8', city: 'Gdansk'},
-    {name: '3 siostry', city: 'Sopot'},
-    {name: 'Lustra', city: 'Gdynia'},
-    {name: 'Wtedy', city: 'Sopot'},
-    {name: 'Stacja delux', city: 'Gdansk'},
-    {name: 'Unique', city: 'Sopot'},
-    {name: 'Dwie zmiany', city: 'Sopot'},
-    {name: 'Scena', city: 'Sopot'},
-    {name: 'TAN', city: 'Sopot'},
-    {name: 'Libation ', city: 'Sopot'},
-];
-var allEvents = [
-    {name: 'Ludzie ulicy', photo: '', data: ''},
-    {name: 'Otwarcie ogródka', photo: '', data: ''},
-    {name: 'Zaćmienie', photo: '', data: ''},
-    {name: 'Cudawianki', photo: '', data: ''},
-    {name: 'Pozytwynie wpłynie TECHNO', photo: ''},
-    {name: 'Ziemia to za mało', photo: '', data: ''},
-    {name: 'Festiwal trafik', photo: '', data: ''},
-    {name: 'Garden party', photo: '', data: ''},
-    {name: 'Wieczor piwa ', photo: '', data: ''},
-    {name: 'Techno Night', photo: '', data: ''},
-    {name: 'House Time', photo: '', data: ''},
-    {name: 'Muzyka elektroniczna', photo: '', data: ''},
-    {name: 'Destroy music', photo: '', data: ''},
-    {name: 'Rozpoczecie seonu', photo: '', data: ''},
-    {name: 'Zmiana miejsc', photo: '', data: ''},
-    {name: 'Wystywa Fotografi', photo: '', data: ''},
-    {name: 'Koncert na dachu', photo: '', data: ''},
-    {name: 'Scena odkryć', photo: '', data: ''},
-    {name: 'VHS HELL', photo: '', data: ''},
-    {name: 'CD HEAVEN', photo: '', data: ''},
-    {name: 'Wieczór', photo: '', data: ''},
-    {name: 'Boat Party', photo: '', data: ''},
-    {name: 'Disco 80', photo: '', data: ''},
-    {name: 'Dance Time', photo: '', data: ''},
+//
+//var allClubs = [
+//    {name: "Bunkier", city: "Gdansk"},
+//    {name: 'Mewa Towarzyska', city: 'Sopot'},
+//    {name: 'Absinthe', city: 'Gdansk'},
+//    {name: 'Sfinks 700', city: 'Sopot'},
+//    {name: 'Kwadratowa', city: 'Gdansk'},
+//    {name: 'Dream Club', city: 'Sopot'},
+//    {name: 'Bunkier', city: 'Gdansk'},
+//    {name: "B90", city: "Gdansk"},
+//    {name: 'Czekoladowa', city: 'Sopot'},
+//    {name: 'SPATiF', city: 'Sopot'},
+//    {name: 'Protokultura', city: 'Gdansk'},
+//    {name: 'Atelier', city: 'Sopot'},
+//    {name: 'HAH', city: 'Sopot'},
+//    {name: 'Wtedy', city: 'Sopot'},
+//    {name: 'Dwie zmiany', city: 'Sopot'},
+//    {name: 'Scena', city: 'Sopot'},
+//    {name: 'TAN', city: 'Sopot'},
+//    {name: 'Libation ', city: 'Sopot'},
+//];
+//var allEvents = [
+//    {name: 'Ludzie ulicy', photo: '', data: ''},
+//    {name: 'Otwarcie ogródka', photo: '', data: ''},
+//    {name: 'Zaćmienie', photo: '', data: ''},
+//    {name: 'Cudawianki', photo: '', data: ''},
+//    {name: 'Pozytwynie wpłynie TECHNO', photo: '', data:''},
+//    {name: 'Ziemia to za mało', photo: '', data: ''},
+//    {name: 'Festiwal trafik', photo: '', data: ''},
+//    {name: 'Garden party', photo: '', data: ''},
+//    {name: 'Wieczor piwa ', photo: '', data: ''},
+//    {name: 'Techno Night', photo: '', data: ''},
+//    {name: 'House Time', photo: '', data: ''},
+//    {name: 'Muzyka elektroniczna', photo: '', data: ''},
+//    {name: 'Destroy music', photo: '', data: ''},
+//    {name: 'Rozpoczecie seonu', photo: '', data: ''},
+//    {name: 'Zmiana miejsc', photo: '', data: ''},
+//    {name: 'Wystywa Fotografi', photo: '', data: ''},
+//    {name: 'Koncert na dachu', photo: '', data: ''},
+//    {name: 'Scena odkryć', photo: '', data: ''},
+//    {name: 'VHS HELL', photo: '', data: ''},
+//    {name: 'CD HEAVEN', photo: '', data: ''},
+//    {name: 'Wieczór', photo: '', data: ''},
+//    {name: 'Boat Party', photo: '', data: ''},
+//    {name: 'Disco 80', photo: '', data: ''},
+//    {name: 'Dance Time', photo:'', data: ''}
+//
+//];
 
-];
+//
+//function generateEvents(events, clubs) {
+//
+//    var newEvents = [];
+//
+//    for (var i = 0; i < 30; i++) {
+//
+//        var dventData= event
+//        var club = clubs[Math.round(Math.random() * allClubs.length - 1)];
+//        var event = events[Math.round(Math.random() * allEvents.length - 1)];
+//        club.eventName = event['name'];
+//        club.eventPhoto = event.photo;
+//        club.eventData = event.data;
+//
+//       //
+//        debugger;
+//
+//        newEvents.push(club);
+//
+//
+//    }
+//    //console.log(newEvents);
+//    return newEvents;
+//}
+//var events = generateEvents(allEvents,allClubs);
+//console.log(events);
+//
+//
+/* do póżniejszego użycia */
 
-
-function generateEvents(events, clubs) {
-
-    var newEvents = [];
-
-    for (var i = 0; i < 25; i++) {
-        var club = clubs[Math.round(Math.random() * allClubs.length - 1)];
-        var event = events[Math.round(Math.random() * allEvents.length - 1)];
-        club.eventName = event['name'];
-        club.eventPhoto = event.photo;
-        club.eventData = event.data;
-
-       //
-
-        newEvents.push(club);
-
-
-    }
-    //console.log(newEvents);
-    return newEvents;
-}
-var events = generateEvents(allEvents,allClubs);
-console.log(events);
-
-
+var eventList=[
+    {name: "Bunkier", city: "Gdansk", nameEvent:'Ludzie ulicy', photo:'images/mini/0.jpg', data: '2016-7-1', id-event="ev-00"},
+    {name: 'Mewa Towarzyska', city: 'Sopot', nameEvent:'Otwarcie ogródka', photo: 'images/mini/1.jpg', data: '2016-7-3', id-event="ev-01"},
+    {name: 'Absinthe', city: 'Gdansk', nameEvent:'Zaćmienie', photo: 'images/mini/2.jpg', data: '2016-7-5', id-event="ev-02"},
+    {name: 'Sfinks 700', city: 'Sopot', nameEvent:'Cudawianki', photo: 'images/mini/3.jpg', data: '2016-7-7', id-event="ev-03"},
+    {name: 'Kwadratowa', city: 'Gdansk', nameEvent:'Festival trafik', photo: 'images/mini/4.jpg', data: '2016-7-9', id-event="ev-04"},
+    {name: 'Dream Club', city: 'Sopot', nameEvent:'Pozytywnie wpłynie techno', photo: 'images/mini/5.jpg', data: '2016-7-12', id-event="ev-05"},
+    {name: 'Bunkier', city: 'Gdansk', nameEvent:'Garden party', photo: 'images/mini/6.jpg', data: '2016-7-14', id-event="ev-06"},
+    {name: "B90", city: "Gdansk", nameEvent:'Techno night', photo: 'images/mini/17.jpg', data: '2016-7-10', id-event="ev-07"},
+    {name: 'Czekoladowa', city: 'Sopot', nameEvent:'House Time', photo: 'images/mini/7.jpg', data: '2016-7-8', id-event="ev-08"},
+    {name: 'SPATiF', city: 'Sopot', nameEvent:'Destyroy Music', photo: 'images/mini/8.jpg', data: '2016-7-9', id-event="ev-09"},
+    {name: 'Protokultura', city: 'Gdansk', nameEvent:'Sunrise', photo: 'images/mini/9.jpg', data: '2016-7-11', id-event="ev-10"},
+    {name: 'Atelier', city: 'Sopot', nameEvent:'Ultra ', photo: 'images/mini/10.jpg', data: '2016-7-13', id-event="ev-11"},
+    {name: 'HAH', city: 'Sopot', nameEvent:'Tomoorowland', photo: 'images/mini/11.jpg', data: '2016-7-15', id-event="ev-12"},
+    {name: 'Wtedy', city: 'Sopot', nameEvent:'Rozpoczęcie sezonu', photo: 'images/mini/12.jpg', data: '2016-7-17', id-event="ev-13"},
+    {name: 'Dwie zmiany', city: 'Sopot', nameEvent:'Scena odkryć', photo: 'images/mini/13.jpg', data: '2016-7-19', id-event="ev-14"},
+    {name: 'Scena', city: 'Sopot', nameEvent:'CD Haven', photo: 'images/mini/14.jpg', data: '2016-7-21', id-event="ev-15"},
+    {name: 'TAN', city: 'Sopot', nameEvent:'CazanTip', photo: 'images/mini/15.jpg', data: '2016-7-23', id-event="ev-16"},
+    {name: 'Libation ', city: 'Sopot', nameEvent:'History Mix ', photo: 'images/mini/16.jpg', data: '2016-7-25', id-event="ev-17"}
+]
