84a28965b027f8b52be4094dc75b9ce115bcbd5f
Msrcin <kurus.mk@gmail.com>
2016-06-17 12:41:07 +0200

    pierwsza dyrektywa

--- new-layout/js/app.js
+++ new-layout/js/app.js
@@ -2,8 +2,22 @@
     var app= angular.module('nazwaApp', []);
 
     app.controller('nazwaControllera', ctrl);
+    app.directive('eventCard', eventCardDirective);
+
     function ctrl($scope){
         $scope.name="Marcin"
 
     }
+
+
+
+    function eventCardDirective(){
+        return {
+            restrict: 'E',
+            templateUrl:'template-ng/eventCard.html'
+        }
+
+    }
+
+
 })();
