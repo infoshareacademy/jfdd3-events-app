bcb04c75e5a0c34d77c211f35685fb82b9b2d5c3
gabrielakrol <gabrieeela.k@gmail.com>
2016-06-19 23:59:20 +0200

    opisy imprez w widoku kalendarza + zmieniajace sie tla

--- new-layout/js/calendar-script.js
+++ new-layout/js/calendar-script.js
@@ -3,13 +3,13 @@
 $('.event-thumbnail').click(function() {
     $('.gridster').removeClass('show');
     $('.gridster').addClass('hide');
-    $('.event-description').removeClass('hide');
-    $('.event-description').addClass('show');
+    $('.event-info').removeClass('hide');
+    $('.event-info').addClass('show');
 });
 
 $('.backward-button').click(function() {
-    $('.event-description').removeClass('show');
-    $('.event-description').addClass('hide');
+    $('.event-info').removeClass('show');
+    $('.event-info').addClass('hide');
     $('.gridster').removeClass('hide');
     $('.gridster').addClass('show');
 });
