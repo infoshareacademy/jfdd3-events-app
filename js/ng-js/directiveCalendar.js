845f9ab63b5d92359bd9a89baf5b444f886e933b
ElwiraKobusiewicz <elwirak4@gmail.com>
2016-06-24 16:43:51 +0200

    naprawa kalendarza

--- js/ng-js/directiveCalendar.js
+++ js/ng-js/directiveCalendar.js
@@ -1,12 +1,75 @@
 eventCalendarApp.directive('appcalendar', function() {
 
+    Date.prototype.addDays = function(days) {
+        var dat = new Date(this.valueOf())
+        dat.setDate(dat.getDate() + days);
+        return dat;
+    };
+
+    function getDates(startDate, stopDate) {
+        var dateArray = new Array();
+        var currentDate = startDate;
+        while (currentDate <= stopDate) {
+            dateArray.push(currentDate)
+            currentDate = currentDate.addDays(1);
+        }
+        return dateArray;
+    }
+
+    function getCalendarDays(begin, howMany) {
+        return getDates(new Date('2016-07-01'), new Date('2016-07-31')).map(function (date, index) {
+
+            function createButton(buttonClasses) {
+                return $('<div>')
+                    .addClass(buttonClasses)
+                    .append( $('<button>')
+                        .addClass('calendar-button')
+                        .text(date)
+                    )
+                    .click(function() {
+                        console.log('date click');
+                    });
+            }
+
+            if (index < 2 + begin) {
+
+                return createButton('col-xs-3 col-sm-2 col-md-2 col-lg-1');
+
+            };
+            if (index >=2 + begin && index < 4 + begin) {
+                return createButton('hidden-xs col-sm-2 col-md-2 col-lg-1');
+
+            };
+            if (index >=4 + begin && index < 10 + begin) {
+
+                return createButton('hidden-xs hidden-sm hidden-md col-lg-1');
+
+            };
+            return date;
+        }).slice(begin, howMany);
+    }
+
+    var pos = 0;
+
     return {
         templateUrl: 'ng-views/calendar.html',
 
         link: function(scope, element, attribute) {
 
-            scope.calendarDays = ['one', 'two', 'three'];
-            $('main div.block').append(calendarDaysHTML);
+
+
+            scope.moveLeft = function () {
+                pos += 1;
+                $('main div.block').empty().append(getCalendarDays(pos, pos + 10));
+            };
+
+            scope.moveRight = function () {
+                pos -= 1;
+                $('main div.block').empty().append(getCalendarDays(pos, pos +  10));
+            };
+
+            //scope.calendarDays = ['one', 'two', 'three'];
+            $('main div.block').empty().append(getCalendarDays(pos, pos + 10));
 
         }
     }
--- new-layout/js/navigation.js
+++ new-layout/js/navigation.js
@@ -8,52 +8,7 @@
 
 
 
-Date.prototype.addDays = function(days) {
-    var dat = new Date(this.valueOf())
-    dat.setDate(dat.getDate() + days);
-    return dat;
-};
 
-function getDates(startDate, stopDate) {
-    var dateArray = new Array();
-    var currentDate = startDate;
-    while (currentDate <= stopDate) {
-        dateArray.push(currentDate)
-        currentDate = currentDate.addDays(1);
-    }
-    return dateArray;
-}
-
-var calendarDaysHTML = getDates(new Date('2016-07-01'), new Date('2016-07-31')).map(function (date, index) {
-
-    function createButton(buttonClasses) {
-        return $('<div>')
-            .addClass(buttonClasses)
-            .append( $('<button>')
-                .addClass('calendar-button')
-                .text(date)
-            )
-            .click(function() {
-                console.log('date click');
-            });
-    }
-
-    if (index < 2) {
-
-        return createButton('col-xs-3 col-sm-2 col-md-2 col-lg-1');
-
-    };
-    if (index >=2 && index < 4) {
-        return createButton('hidden-xs col-sm-2 col-md-2 col-lg-1');
-
-    };
-    if (index >=4 && index < 10) {
-
-        return createButton('hidden-xs hidden-sm hidden-md col-lg-1');
-
-    };
-    return date;
-});
 
 //$('main div.block').append(result);
 $(document).ready(function() {
